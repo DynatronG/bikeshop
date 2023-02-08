9d32ad13ca4b9f6d76328d76bcd4b7dcd8efbf9a
DynatronG <firefog@mail.ru>
2023-02-09 09:17:25 +1000

    002

--- src/components/headerBlock.vue
+++ src/components/headerBlock.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <div class="head">
-      <img id="idImgHead" class="imgHead" v-bind:src="imgHeadPath" />
+      <div class="imgHead"></div>
     </div>
     <nav-block></nav-block>
   </header>
@@ -12,33 +12,9 @@ import NavBlock from "./navBlock.vue";
 export default {
   components: { NavBlock },
   data() {
-    return {
-      imagesHead: {
-        big: "headImg_1920-634.png",
-        small: "headImg_1920_small.png",
-      },
-      imgHeadPath: require("@/assets/img/headImg_1920-634.png"),
-      screenWidth: 0,
-    };
+    return {};
   },
-  methods: {
-    changeImg() {
-      this.screenWidth = window.innerWidth;
-      if (this.screenWidth < 1200) {
-        this.imgHeadPath = require("@/assets/img/" + this.imagesHead.small);
-      } else {
-        this.imgHeadPath = require("@/assets/img/" + this.imagesHead.big);
-      }
-    },
-    updateWidth() {
-      this.screenWidth = window.innerWidth;
-      // this.screenWidth = document.documentElement.clientWidth;
-      console.log("Изменен размер экрана");
-    },
-  },
-  mounted() {
-    window.addEventListener("resize", this.changeImg);
-  },
+  methods: {},
 };
 </script>
 
